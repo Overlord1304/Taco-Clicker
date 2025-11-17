2ee46a00ae3a5cc5f2b60830e9984c552ea125d7
unknown <overlordxanime0@gmail.com>
2025-11-17 17:39:16 +0400

    made ui look better and working on animations of taco

---
+++ scripts/anims.gd
@@ -0,0 +1,10 @@
+extends MarginContainer
+
+
+
+func _on_tacobutton_button_down() -> void:
+	pass # Replace with function body.
+
+
+func _on_tacobutton_button_up() -> void:
+	pass # Replace with function body.
