6c25e8a5d756055996b51b087e0296d195a8684e
unknown <overlordxanime0@gmail.com>
2025-11-18 12:09:50 +0400

    added click upgrades and working on passive clicks gained

--- scripts/game.gd
+++ scripts/game.gd
@@ -3,12 +3,18 @@ extends Control
 const saves =  "user://userdata.save"
 var tacos = 0
 var amount_per_click = 1
+var passive_gains = 0
 
 signal tacos_changed
 signal taco_clicked
 func _ready():
 	load_data()
 	emit_signal("tacos_changed", tacos)
+	
+func _process(_delta):
+	await get_tree().create_timer(1.0).timeout
+	tacos += passive_gains
+	save_data()
 func _on_tacobutton_button_down() -> void:
 	tacos += amount_per_click
 	emit_signal("tacos_changed",tacos)
@@ -17,7 +23,9 @@ func _on_tacobutton_button_down() -> void:
 
 func save_data():
 	var data = {
-		"tacos" : tacos
+		"tacos" : tacos,
+		"amount_per_click": amount_per_click,
+		"passive_gains" : passive_gains
 	}
 	var file = FileAccess.open(saves, FileAccess.WRITE)
 	file.store_var(data)
@@ -30,5 +38,22 @@ func load_data():
 		file.close()
 		if typeof(data) == TYPE_DICTIONARY:
 			tacos = data.get("tacos",0)
+			amount_per_click = data.get("amount_per_click",1)
+			passive_gains = data.get("passive_gains",0)
 	else:
 		save_data()
+
+func _on_button_button_down() -> void:
+	if tacos >= 35: 
+		amount_per_click += 1
+		tacos -= 35
+		emit_signal("tacos_changed",tacos)
+		save_data() 
+
+
+func _on_autoclickupg_1_button_down() -> void:
+	if tacos >= 100: 
+		passive_gains += 1
+		tacos -= 100
+		emit_signal("tacos_changed",tacos)
+		save_data() 
--- scripts/indicator_fade_out.gd
+++ scripts/indicator_fade_out.gd
@@ -2,4 +2,6 @@ extends Label
 
 
 func _on_timer_timeout() -> void:
-	queue_free()
+	var tween = create_tween()
+	tween.tween_property(self, "modulate:a", 0.0, 0.5) 
+	tween.tween_callback(Callable(self, "queue_free"))
