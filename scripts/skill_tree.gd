03a8486daa9f59c69012a786ce6c5d965699a4fc
unknown <overlordxanime0@gmail.com>
2025-12-31 17:35:47 +0400

    committing just for the hourly commit, making good progress on skill tree tho

--- scripts/skill_tree.gd
+++ scripts/skill_tree.gd
@@ -2,6 +2,7 @@ extends Control
 var hyperdrive_cost = 1_000_000_000
 var gtacomax_cost = 10_000_000
 var dsaucemax_cost = 20_000_000
+var coverflow_cost = 30_000_000
 func _process(_delta):
 	
 	if Global.hyperdrive_bought:
@@ -9,12 +10,16 @@ func _process(_delta):
 		$Panel/OverDrive/Panel/buyhyperdrive.disabled = true
 		$Panel/GTacoUpg.disabled = false
 		$Panel/DSauceUpg.disabled = false
+		$Panel/COverflowUpg.disabled = false
 	if Global.gtacomax_bought:
 		$Panel/GTacoUpg/Panel/buygtacomax.text = "BOUGHT"
 		$Panel/GTacoUpg/Panel/buygtacomax.disabled = true
 	if Global.dsaucemax_bought:
 		$Panel/DSauceUpg/Panel/buydsaucemax.text = "BOUGHT"
 		$Panel/DSauceUpg/Panel/buydsaucemax.disabled = true
+	if Global.coverflowmax_bought:
+		$Panel/COverflowUpg/Panel/buycoverflowmax.text = "BOUGHT"
+		$Panel/COverflowUpg/Panel/buycoverflowmax.disabled = true
 func _on_back_button_up():
 	var swipe = get_meta("swipe")
 	if swipe:
@@ -27,6 +32,7 @@ func _on_over_drive_button_up() -> void:
 		$Panel/OverDrive/Panel.visible = true
 		$Panel/GTacoUpg/Panel.visible = false
 		$Panel/DSauceUpg/Panel.visible = false
+		$Panel/COverflowUpg/Panel.visible = false
 		
 
 func _on_buyhyperdrive_button_up() -> void:
@@ -50,6 +56,7 @@ func _on_g_taco_upg_button_up() -> void:
 		$Panel/GTacoUpg/Panel.visible = true
 		$Panel/OverDrive/Panel.visible = false
 		$Panel/DSauceUpg/Panel.visible = false
+		$Panel/COverflowUpg/Panel.visible = false
 
 
 func _on_buygtacomax_button_up() -> void:
@@ -72,7 +79,7 @@ func _on_d_sauce_upg_button_up() -> void:
 		$Panel/DSauceUpg/Panel.visible = true
 		$Panel/GTacoUpg/Panel.visible = false
 		$Panel/OverDrive/Panel.visible = false
-
+		$Panel/COverflowUpg/Panel.visible = false
 
 func _on_buydsaucemax_button_up() -> void:
 	var game = get_tree().get_first_node_in_group("game")
@@ -85,3 +92,22 @@ func _on_buydsaucemax_button_up() -> void:
 		game.recalc()
 	else:
 		game.show_cost_warning($notenoughmoneylabel)
+
+
+
+
+
+func hide_all_panels():
+	for panel in get_tree().get_nodes_in_group("upg_desc"):
+		panel.visible = false
+
+
+
+
+func _on_panel_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		hide_all_panels()
+
+
+func _on_c_overflow_upg_button_up() -> void:
+	pass # Replace with function body.
